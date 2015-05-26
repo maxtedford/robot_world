fe39859e60bd153283c16db3935ce2001792fb76
Max Tedford <max.tedford@gmail.com>
2015-05-26 09:20:00 -0600

    app now fully running--still want to get the avatar functionality going though

--- db/migrations/001_create_robots_table.rb
+++ db/migrations/001_create_robots_table.rb
@@ -1,7 +1,7 @@
 require 'sequel'
 require_relative '../../app/models/robot_world'
 
-TaskManager.database.create_table(:robots) do
+RobotWorld.database.create_table(:robots) do
   primary_key :id
   String      :name
   String      :city
